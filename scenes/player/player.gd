1d00706c62cf62b1ff12802e15e2284a718c3554
WinterAtne <Winter54b@gmail.com>
2024-02-28 20:41:45 -0700

    Improved movement system

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,24 +1,65 @@
 extends CharacterBody2D
 
+#Parameters
+var speed : float = 300.0
+var acceleration : float = 1200
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var jump_velocity : float = -600.0
+var jump_cancel_multiple : float = .3333333
+var gravity : float = ProjectSettings.get_setting("physics/2d/default_gravity")
+var max_fall_velocity : float = 1200
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+#State
+var jump_buffered : bool = false
+var jump_canceled : bool = false
 
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	
-	if Input.is_action_pressed("up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	vertical_movement(delta)
+	horizontal_movement(delta)
 	
 	move_and_slide()
+	
+
+func horizontal_movement(delta : float) -> void:
+	var direction = Input.get_axis("left", "right")
+	
+	if direction:
+		velocity.x = move_toward(velocity.x, speed * direction, delta * acceleration)
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, delta * acceleration)
+		
+	
+
+func vertical_movement(delta : float) -> void:
+	#Input:
+	if Input.is_action_just_pressed("up"):
+		jump_buffered = true
+		jump_canceled = false
+		
+	elif Input.is_action_just_released("up"):
+		jump_buffered = false
+		jump_canceled = true
+		
+	
+	#Actual Movement
+	if is_on_floor():
+		if jump_buffered:
+			velocity.y = jump_velocity
+			jump_buffered = false
+			
+		
+		return
+		
+	
+	if jump_canceled and velocity.y < 0:
+		velocity.y *= jump_cancel_multiple
+		jump_canceled = false
+		return
+		
+	
+	#Ensures we don't start falling really fast
+	if velocity.y < max_fall_velocity:
+		velocity.y += gravity * delta
+		
+	
