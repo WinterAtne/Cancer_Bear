857681643d220b274482bd54e40b0115de22fdee
WinterAtne <Winter54b@gmail.com>
2024-03-02 15:08:39 -0700

    Created i-frames

--- scenes/damage_system/health_profile.gd
+++ scenes/damage_system/health_profile.gd
@@ -2,3 +2,4 @@ extends Resource
 class_name Health_Profile
 
 @export var max_health : int = 1
+@export var invulnerable_seconds : float = -1 #If set to -1 the attached object does not have i-frames
--- scenes/damage_system/hit_box.gd
+++ scenes/damage_system/hit_box.gd
@@ -5,9 +5,12 @@ class_name HitBox
 
 signal hit(damage : int,  health : int, normal : Vector2, knockback : float)
 signal died
+signal invulnerable
+signal not_invulnerable
 
 var current_health : int = -1
 var health_is_ready : bool = false
+var can_take_damage : bool = true
 
 func _ready() -> void:
 	await owner.ready # We get our health_profile from the owner
@@ -21,6 +24,10 @@ func _on_area_entered(area) -> void:
 			await owner.ready
 			
 		
+		if !can_take_damage:
+			print("cannot take damage")
+			return
+		
 		current_health -= area.damage
 		hit.emit(area.damage, 
 			current_health, 
@@ -29,6 +36,8 @@ func _on_area_entered(area) -> void:
 		if current_health <= 0:
 			died.emit()
 			
+		
+		manage_invulnerable()
 		
 	
 
@@ -40,4 +49,14 @@ func heal(amount : int) -> void:
 	current_health += amount
 	if current_health > health_profile.max_health:
 		current_health = health_profile.max_health
+	
+
+func manage_invulnerable():
+	if health_profile.invulnerable_seconds != -1:
+		can_take_damage = false
+		invulnerable.emit()
+		await get_tree().create_timer(health_profile.invulnerable_seconds).timeout
+		not_invulnerable.emit()
+		can_take_damage = true#Changed after signal so state elsewhere can communicate to the player
+		
 	
