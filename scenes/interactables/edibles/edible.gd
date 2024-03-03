b350c4655c1ed6293cfbf00a37541a4ff8ebfeee
WinterAtne <Winter54b@gmail.com>
2024-03-03 15:24:53 -0700

    Updated interactable and made edibles

---
+++ scenes/interactables/edibles/edible.gd
@@ -0,0 +1,10 @@
+extends Interactable
+class_name Edible
+
+func _do_interaction():
+	if should_interact:
+		print("hey")
+		
+	
+	is_interactable = false
+	
--- scenes/interactables/interactable.gd
+++ scenes/interactables/interactable.gd
@@ -1,10 +1,30 @@
 extends Area2D
+class_name Interactable
 
+var is_interactable : bool = false
+var is_interacting : bool = false
+var should_interact : bool = false
+
+func _process(_delta) -> void:
+	is_interacting = Input.is_action_pressed("interact")
+	should_interact = is_interacting and is_interactable
+	if should_interact:
+		_do_interaction()
+		
+	
 
 #This is a virtual function
-func _interact(body : Node2D) -> void:
-	if not body is Player:
-		return
+func _do_interaction() -> void:
+	pass
+
+func _on_body_entered(body : Node2D) -> void:
+	if body is Player:
+		is_interactable = true
+		
 	
-	print(body)
+
+func _on_body_exited(body):
+	if body is Player:
+		is_interactable = false
+		
 	
