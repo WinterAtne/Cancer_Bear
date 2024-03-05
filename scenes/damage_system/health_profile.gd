d27ca3d1b61ed24c505099e73628fa2c1a340ef8
WinterAtne <Winter54b@gmail.com>
2024-03-04 20:57:57 -0700

    Added velocity damage

--- scenes/damage_system/health_profile.gd
+++ scenes/damage_system/health_profile.gd
@@ -3,3 +3,4 @@ class_name Health_Profile
 
 @export var max_health : int = 1
 @export var invulnerable_seconds : float = -1 #If set to -1 the attached object does not have i-frames
+@export var velocity_damage_threshold : float = 700 #If set to -1 the object does not take fall damage
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -8,7 +8,7 @@ var acceleration : float = 1200
 var jump_velocity : float = -650.0
 var jump_cancel_multiple : float = .3333333
 var gravity : float = ProjectSettings.get_setting("physics/2d/default_gravity") * 1.34
-var max_fall_velocity : float = 900
+var max_fall_velocity : float = 2000
 
 @onready var camera : Camera2D = %Camera2D
 @onready var spell_caster : SpellCaster = %SpellCaster
