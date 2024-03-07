ff887a66c29be7f55fd724e2c671e69185033eb6
WinterAtne <Winter54b@gmail.com>
2024-03-06 22:17:34 -0700

    Added particles to claw

--- scenes/spells/claw/claw.gd
+++ scenes/spells/claw/claw.gd
@@ -2,6 +2,11 @@ extends TempObject
 
 func _ready():
 	reparent(PlayerData.player_instance)
+	$GPUParticles2D.emitting = true;
+	if not PlayerData.player_instance.facing_right:
+		scale.x = -1
+		
+	
 	await get_tree().process_frame # we wait to make sure the reparent happens
 	position = position + ((1 if PlayerData.player_instance.facing_right else -1) * Vector2.RIGHT * 48)
 	super._ready()
